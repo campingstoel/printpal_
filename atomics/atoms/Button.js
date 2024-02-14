d84cf9d1986b47f9665c1aaa63b5c52ea3d354aa
Camryn Terlouw <cterlouw04@gmail.com>
2024-02-14 19:32:07 +0100

    feat(atomics): added certain atoms as base

--- atomics/atoms/Button.js
+++ atomics/atoms/Button.js
@@ -1,28 +1,15 @@
-import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import button from "../../styles/button";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-export default function Button({style, text, onPress}) {
-    const navigation = useNavigation();
-    return (
-        <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
-            <Text style={styles.text}>{text}</Text>
-        </TouchableOpacity>
-    );
+export default function Button({ style, text, onPress, icon }) {
+  const navigation = useNavigation();
+
+  return (
+    <TouchableOpacity style={[button.button, style]} onPress={onPress}>
+      <Ionicons name={icon} size={30} color="white" />
+    </TouchableOpacity>
+  );
 }
-
-const styles = StyleSheet.create({
-    button: {
-        backgroundColor: 'blue',
-        padding: 10,
-        borderRadius: 5,
-        margin: 10,
-    },
-    wide: {
-    },
-    text: {
-        color: 'white',
-        textAlign: 'center',
-    },
-});
-
--- atomics/pages/Home.js
+++ atomics/pages/Home.js
@@ -1,13 +1,17 @@
-import { View, Text } from "react-native";
+import { TouchableOpacity, Text, StyleSheet, View } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
 import Button from "../atoms/Button";
+import button from '../../styles/button';
+import Header from '../atoms/Header';
+import header from '../../styles/header';
 
 export default function Home() {
   const navigation = useNavigation();
   return (
     <View>
-      <Text>Home</Text>
-        <Button text="Go to About" onPress={() => navigation.navigate("About")} />
+      <Header style={header.header} text={"Home"} />
+        <Button icon={'add-outline'} onPress={() => navigation.navigate("About")} style={[button.thumb, button.circle]} />
     </View>
   );
 }
+
