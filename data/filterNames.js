35740e16a7d4c4a38be58303fb6662a9b89a8f53
Camryn Terlouw <cterlouw04@gmail.com>
2024-03-11 21:47:41 +0100

    fix(filters): fixed dynamic import error

--- data/filterNames.js
+++ data/filterNames.js
@@ -3,21 +3,20 @@ export const filterNames = [
         id: 1,
         text: "A4 paper",
         important: false,
-        image: `${require('../images/document.png')}`,
+        image: require('../images/document.png'),
 
     },
     {
         id: 2,
         text: "A3 paper",
         important: false,
-        image: `${require('../images/newspaper.png')}`,
-
+        image: require('../images/newspaper.png'),
     },
     {
         id: 3,
         text: "Poster",
         important: false,
-        image: `${require('../images/poster-icon.png')}`,
+        image: require('../images/poster-icon.png'),
 
         
     },
@@ -25,20 +24,20 @@ export const filterNames = [
         id: 4,
         text: "Color",
         important: false,
-        image: `${require('../images/poster.png')}`,
+        image: require('../images/poster.png'),
 
     },
     {
         id: 5,
         text: "B&W",
         important: false,
-        image: `${require('../images/varnish.png')}`,
+        image: require('../images/varnish.png'),
 
     },
     {
         id: 6,
         text: "Lamination",
         important: false,
-        image: `${require('../images/lamination.png')}`,
+        image: require('../images/lamination.png'),
     }
 ];
